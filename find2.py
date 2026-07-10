6e0cc5384c13f1b8a7a1b67eb8d9d3abe52427f8
Adityajs1 <aditya96937@gmail.com>
2026-07-10 23:22:43 +0530

    update

--- find2.py
+++
@@ -1,6 +0,0 @@
-with open('site3.html', 'r', encoding='utf-8') as f:
-    lines = f.readlines()
-
-for i, line in enumerate(lines):
-    if 'cx="9.5"' in line or 'cards-grid' in line:
-        print(f'{i+1}: {line.strip()[:100]}')
